31df90f78cdb68dc5709137dc347f89d34f7e035
pierre <jobrider@gmail.com>
2023-06-18 21:54:00 +0200

    rajout d'un champ ville, fetch et affichage

--- meteo.js
+++ meteo.js
@@ -1,5 +1,19 @@
 const ville = 'paris'
 
+// Récupérer le champ input et le bouton de recherche par leur ID
+const villeInput = document.getElementById('villeInput');
+const rechercherBtn = document.getElementById('rechercherBtn');
+
+rechercherBtn.addEventListener('click', () => {
+    
+    const ville = villeInput.value;
+    //console.log('ville1 : ', ville);
+    fetchmaVille(ville)
+})
+//console.log('ville2 : ', ville);
+
+function fetchmaVille(ville){
+
 fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&APPID=419820eaf37691256046f0c1301711de&units=metric&lang=fr`)
   .then(response => response.json())
   .then(data => {
@@ -12,3 +26,6 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&APPID=419820eaf
   .catch(error => {
     console.log('Une erreur s\'est produite :', error);
   });
+}
+
+fetchmaVille(ville)
