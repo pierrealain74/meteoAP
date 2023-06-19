debb8ab50ce2b1476417dfdca6efb21201645b52
pierre <jobrider@gmail.com>
2023-06-19 10:59:03 +0200

    css design avec le carré centrale et le moteur recherche via boostrap

--- meteo.js
+++ meteo.js
@@ -1,6 +1,6 @@
 const ville = 'paris'
 
-import { apicode } from './config.js';
+import { apicode } from './config.js';//Le codeapi y est stocké
 
 // Récupérer le champ input et le bouton de recherche par leur ID
 const villeInput = document.getElementById('villeInput');
@@ -22,7 +22,21 @@ fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ville}&APPID=${apicode
     console.log(data); // Affiche les données de la réponse dans la console
 
     const mainElement = document.querySelector('main');
-      mainElement.innerHTML = `<p>Température à ${data.name} : ${data.main.temp} °C</p><p>Température ressentie : ${data.main.feels_like} °C</p><p>Vent : ${data.wind.speed}km/h</p>`;
+
+    const villeElement = document.querySelector('.ville p');
+    villeElement.innerHTML = `<span class="ville">${ville}</span>`
+
+    const tempElement = document.querySelector('.temp p');
+    tempElement.innerHTML = `<span class="temp">${data.main.temp} °C</span>`
+
+    const humidityElement = document.querySelector('.humidity p');
+    humidityElement.innerHTML = `<span class="humidity">${data.main.humidity} %</span>`
+
+    const windElement = document.querySelector('.wind p');
+    windElement.innerHTML = `<span class="wind">${data.wind.speed}km/h</span>`
+
+
+/*       mainElement.innerHTML = `<p>Ville : ${ville}</p><p>Température à ${data.name} : ${data.main.temp} °C</p><p>Température ressentie : ${data.main.feels_like} °C</p><p>Vent : ${data.wind.speed}km/h</p>`; */
       
   })
   .catch(error => {
